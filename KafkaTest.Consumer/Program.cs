0b9c983e19d61fc9f1744cbf436136962718a975
Túlio Paim <tutpaim@gmail.com>
2022-01-04 09:42:54 -0300

    Add project files.

---
+++ KafkaTest.Consumer/Program.cs
@@ -0,0 +1,48 @@
+﻿using Confluent.Kafka;
+
+var conf = new ConsumerConfig
+{
+    GroupId = "test-consumer-group",
+    BootstrapServers = "127.0.0.1:9092",
+    AutoOffsetReset = AutoOffsetReset.Earliest,
+};
+
+using var consumer = new ConsumerBuilder<Ignore, string>(conf).Build();
+
+consumer.Subscribe("topico_teste");
+
+Console.WriteLine($"Consumidor online");
+
+var cts = new CancellationTokenSource();
+Console.CancelKeyPress += (_, e) =>
+{
+    e.Cancel = true;
+    cts.Cancel();
+};
+
+var messageCount = 0;
+
+try
+{
+    while (true)
+    {
+        try
+        {
+            var result = consumer.Consume(cts.Token);
+
+            var message = $"Recebida '{result.Message.Value}' '" +
+                          $"Partition: {result.Partition.Value} " +
+                          $"| MessageNumber: {++messageCount}'\n";
+
+            Console.WriteLine(message);
+        }
+        catch (ConsumeException e)
+        {
+            Console.WriteLine($"Error occured: {e.Error.Reason}");
+        }
+    }
+}
+catch (OperationCanceledException)
+{
+    consumer.Close();
+}
---
+++ KafkaTest.Producer/Program.cs
@@ -0,0 +1,31 @@
+﻿using Confluent.Kafka;
+
+var config = new ProducerConfig { BootstrapServers = "127.0.0.1:9092" };
+
+using var p = new ProducerBuilder<Null, string>(config).Build();
+
+try
+{
+    while (true)
+    {
+        Console.WriteLine("Aguardando tecla...");
+        Console.ReadKey();
+
+        for (var count = 1; count <= 300; count++)
+        {
+            var message = "oi";
+
+            var dr = await p.ProduceAsync(
+                "topico_teste",
+                new Message<Null, string> { Value = message });
+
+            Console.WriteLine($"Publicada {message} "+ 
+                              $"'Partition: {dr.Partition.Value} "+
+                              $"| MessageNumber: {count}'\n");
+        }
+    }
+}
+catch (ProduceException<Null, string> e)
+{
+    Console.WriteLine($"Delivery failed: {e.Error.Reason}");
+}
